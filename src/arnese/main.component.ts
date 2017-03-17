188e900bf00d280df0d299acf57fc5736cfbb379
Martin-Peter Lakatos <m16peter@yahoo.ro>
2017-03-17 20:43:38 +0100

    ...

--- src/arnese/main.component.ts
+++ src/arnese/main.component.ts
@@ -6,11 +6,10 @@ import { Component, HostListener, ViewChild } from '@angular/core';
     styleUrls: ['./main.less']
 })
 
+/**
+ * Manage app dimensions & scroll
+ */
 export class MainComponent {
-
-    /**
-     * Manage app width, height & scroll
-     */
 
     // bind elements
     @ViewChild('container') container;
@@ -19,59 +18,58 @@ export class MainComponent {
 
     // listen for scroll
     @HostListener('window:scroll', ['$event']) onScroll(ev) {
-        this.setScroller();
+        this.scroll();
     }
 
     // listen for resize
     @HostListener('window:resize', ['$event']) onResize(ev) {
-        this.setDimensions();
-        this.setScroller();
+        this.resize();
+        this.scroll();
     }
 
     // init width/height
     ngAfterViewInit() {
-        this.setDimensions();
-        this.setScroller();
+        this.resize();
+        this.scroll();
     }
 
-    // on resize, fix width/height
-    private setDimensions(): void {
+    /**
+     * Set dimensions
+     */
+    private resize(): void {
 
-        // set width
-        let w = this.container.nativeElement.offsetWidth + 'px solid transparent';
+        // set header & footer shape
+        let w = this.container.nativeElement.offsetParent.offsetWidth + 'px solid transparent';
         this.header.nativeElement.style.borderRight = w;
         this.footer.nativeElement.style.borderLeft = w;
 
-        // set height
+        // set container's element height to 100% & 100px overflow
         let h =  this.container.nativeElement.offsetParent.parentElement.clientHeight;
         this.container.nativeElement.style.height = h + 50 + "px";
 
     }
 
-    // on scroll, hide/show header/footer
-    private setScroller(): void {
+    /**
+     * Animate Scroll
+     */
+    private scroll(): void {
 
+        // container elements height
         let element = this.container.nativeElement.clientHeight;
+        // browsers height
         let browser = this.container.nativeElement.offsetParent.parentElement.clientHeight;
+        // scrolled size
         let scrolled = this.container.nativeElement.offsetParent.scrollTop;
 
-        let total;
-        let percentage;
-        let height;
+        // trnsform container elements height to percentage
+        let percentage = (browser > element) ? 100 : Math.floor((scrolled * 100) / (element - browser) );
+        // transform percentage to scrolled size
+        let size = Math.floor(( (browser/2 - 100) * percentage) / 100);
 
-        if ( browser > element ) {
-            percentage = 100;
-        } else {
-            total = element - browser;
-            percentage = Math.floor((scrolled * 100) / total);
-        }
+        // position the header & footer based on scroll size
+        this.header.nativeElement.offsetParent.style.transform = "translateY(-" + size + "px)";
+        this.footer.nativeElement.offsetParent.style.transform = "translateY(" + size + "px)";
 
-        total = browser/2 - 100;
-        height = Math.floor((total * percentage) / 100);
-        height = (total - height) + "px";
-
-        this.header.nativeElement.offsetParent.style.height = height;
-        this.footer.nativeElement.offsetParent.style.height = height;
     }
 
 }
