f3b72bbbae609f5a5086133c7c76b2546bae6e8e
Martin-Peter Lakatos <m16peter@yahoo.ro>
2017-03-27 07:36:48 +0200

    [*] firefox

--- src/arnese/app-main.ts
+++ src/arnese/app-main.ts
@@ -3,143 +3,141 @@ import { SK } from './lang';
 
 @Component({
     selector: 'app-main',
-    template: '<div><h1 *ngFor="let item of browsers">{{ item }}</h1></div>',
-    // templateUrl: './app-main.html',
-    // styleUrls: ['./app-main.less']
+    templateUrl: './app-main.html',
+    styleUrls: ['./app-main.less']
 })
 
 export class MainComponent implements AfterViewInit {
 
     public sk = SK;
-    public browsers;
+    browsers;
 
-    //
-    // @ViewChild('container') container;
-    // @ViewChild('header') header;
-    // @ViewChild('content') content;
-    // @ViewChild('page') page;
-    // @ViewChild('footer') footer;
-    //
-    // @HostListener('window:scroll', ['$event']) onScroll() {
-    //     this.scroll();
-    // }
-    //
-    // @HostListener('window:resize', ['$event']) onResize() {
-    //     this.resize();
-    //     this.scroll();
-    // }
-    //
+    @ViewChild('container') container;
+    @ViewChild('header') header;
+    @ViewChild('content') content;
+    @ViewChild('page') page;
+    @ViewChild('footer') footer;
+
+    @HostListener('window:scroll', ['$event']) onScroll() {
+        this.scroll();
+    }
+
+    @HostListener('window:resize', ['$event']) onResize() {
+        this.resize();
+        this.scroll();
+    }
+
     ngAfterViewInit() {
         this.detectBrowsers();
 
-        // this.resize();
-        // this.scrollTop();
+        this.resize();
+        this.scrollTop();
     }
-    //
-    // private resize(): void {
-    //     this.header.nativeElement.style.borderRight = this.container.nativeElement.offsetParent.offsetWidth + 'px solid transparent';
-    //     this.page.nativeElement.style.marginTop = this.container.nativeElement.offsetParent.offsetHeight + 'px';
-    //     this.footer.nativeElement.style.borderLeft = this.container.nativeElement.offsetParent.offsetWidth + 'px solid transparent';
-    // }
-    //
-    // /**
-    //  * Scroll
-    //  */
-    // private scroll(): void {
-    //     // .parentElement
-    //     let size = this.container.nativeElement.offsetParent.scrollTop;
-    //
-    //     if (size >= 99) {
-    //
-    //         this.header.nativeElement.offsetParent.style.transform = 'translateY(-100%)';
-    //         this.header.nativeElement.style.borderTop = '51px solid #333333';
-    //
-    //         this.content.nativeElement.style.display = 'none';
-    //
-    //         this.footer.nativeElement.offsetParent.style.transform = 'translateY(100%)';
-    //         this.footer.nativeElement.style.borderBottom = '51px solid #333333';
-    //
-    //     } else {
-    //
-    //         this.header.nativeElement.offsetParent.style.transform = 'translateY(-' + size + '%)';
-    //         this.header.nativeElement.style.borderTop = '51px solid #fff';
-    //
-    //         this.content.nativeElement.style.display = 'flex';
-    //         this.content.nativeElement.style.opacity = '0.' + (1 - Number('0.' + Math.floor(size < 30 ? 0 : size) ));
-    //
-    //         this.footer.nativeElement.offsetParent.style.transform = 'translateY(' + size + '%)';
-    //         this.footer.nativeElement.style.borderBottom = '51px solid #fff';
-    //
-    //     }
-    //
-    // }
-    // public scrollTop(): void {
-    //
-    //     let start = this.container.nativeElement.offsetParent.scrollTop + 1;
-    //     let end = 0;
-    //
-    //     let interval = setInterval(() => {
-    //
-    //         start = Math.floor(start / 1.1);
-    //
-    //         if (start > end) {
-    //             this.container.nativeElement.offsetParent.scrollTop = start;
-    //             this.scroll();
-    //         } else {
-    //             this.container.nativeElement.offsetParent.scrollTop = end;
-    //             this.scroll();
-    //             clearInterval(interval);
-    //         }
-    //
-    //     }, 10);
-    //
-    // }
-    // public scrollPageStart(): void {
-    //
-    //     let start = this.container.nativeElement.offsetParent.scrollTop + 1;
-    //     let end = this.container.nativeElement.offsetParent.offsetHeight / 2;
-    //     let step = end;
-    //
-    //     let interval = setInterval(() => {
-    //
-    //         step = Math.floor(step / 1.001);
-    //         start += (end - step);
-    //
-    //         if (start < end) {
-    //             this.container.nativeElement.offsetParent.scrollTop = start;
-    //         } else {
-    //             this.container.nativeElement.offsetParent.scrollTop = end;
-    //             clearInterval(interval);
-    //         }
-    //
-    //     }, 10);
-    //
-    // }
-    // public scrollPageEnd(): void {
-    //
-    //     let start = this.container.nativeElement.offsetParent.scrollTop;
-    //     let end = this.container.nativeElement.offsetParent.scrollHeight - this.container.nativeElement.offsetParent.offsetHeight;
-    //     let step = end;
-    //
-    //     let interval = setInterval(() => {
-    //
-    //         step = Math.floor(step / 1.001);
-    //         start += (end - step);
-    //
-    //         if (start < end) {
-    //             this.container.nativeElement.offsetParent.scrollTop = start;
-    //         } else {
-    //             this.container.nativeElement.offsetParent.scrollTop = end;
-    //             clearInterval(interval);
-    //         }
-    //
-    //     }, 10);
-    //
-    // }
-    //
-    // public img(filename: string) : string {
-    //     return 'assets/img/' + filename;
-    // }
+
+    private resize(): void {
+        this.header.nativeElement.style.borderRight = this.container.nativeElement.offsetParent.parentElement.offsetWidth + 'px solid transparent';
+        this.page.nativeElement.style.marginTop = this.container.nativeElement.offsetParent.parentElement.offsetHeight + 'px';
+        this.footer.nativeElement.style.borderLeft = this.container.nativeElement.offsetParent.parentElement.offsetWidth + 'px solid transparent';
+    }
+
+    /**
+     * Scroll
+     */
+    private scroll(): void {
+        // .parentElement
+        let size = this.container.nativeElement.offsetParent.parentElement.scrollTop;
+
+        if (size >= 99) {
+
+            this.header.nativeElement.offsetParent.style.transform = 'translateY(-100%)';
+            this.header.nativeElement.style.borderTop = '51px solid #333333';
+
+            this.content.nativeElement.style.display = 'none';
+
+            this.footer.nativeElement.offsetParent.style.transform = 'translateY(100%)';
+            this.footer.nativeElement.style.borderBottom = '51px solid #333333';
+
+        } else {
+
+            this.header.nativeElement.offsetParent.style.transform = 'translateY(-' + size + '%)';
+            this.header.nativeElement.style.borderTop = '51px solid #fff';
+
+            this.content.nativeElement.style.display = 'flex';
+            this.content.nativeElement.style.opacity = '0.' + (1 - Number('0.' + Math.floor(size < 30 ? 0 : size) ));
+
+            this.footer.nativeElement.offsetParent.style.transform = 'translateY(' + size + '%)';
+            this.footer.nativeElement.style.borderBottom = '51px solid #fff';
+
+        }
+
+    }
+    public scrollTop(): void {
+
+        let start = this.container.nativeElement.offsetParent.parentElement.scrollTop + 1;
+        let end = 0;
+
+        let interval = setInterval(() => {
+
+            start = Math.floor(start / 1.1);
+
+            if (start > end) {
+                this.container.nativeElement.offsetParent.parentElement.scrollTop = start;
+                this.scroll();
+            } else {
+                this.container.nativeElement.offsetParent.parentElement.scrollTop = end;
+                this.scroll();
+                clearInterval(interval);
+            }
+
+        }, 10);
+
+    }
+    public scrollPageStart(): void {
+
+        let start = this.container.nativeElement.offsetParent.parentElement.scrollTop + 1;
+        let end = this.container.nativeElement.offsetParent.parentElement.offsetHeight / 2;
+        let step = end;
+
+        let interval = setInterval(() => {
+
+            step = Math.floor(step / 1.001);
+            start += (end - step);
+
+            if (start < end) {
+                this.container.nativeElement.offsetParent.parentElement.scrollTop = start;
+            } else {
+                this.container.nativeElement.offsetParent.parentElement.scrollTop = end;
+                clearInterval(interval);
+            }
+
+        }, 10);
+
+    }
+    public scrollPageEnd(): void {
+
+        let start = this.container.nativeElement.offsetParent.parentElement.scrollTop;
+        let end = this.container.nativeElement.offsetParent.parentElement.scrollHeight - this.container.nativeElement.offsetParent.parentElement.offsetHeight;
+        let step = end;
+
+        let interval = setInterval(() => {
+
+            step = Math.floor(step / 1.001);
+            start += (end - step);
+
+            if (start < end) {
+                this.container.nativeElement.offsetParent.parentElement.scrollTop = start;
+            } else {
+                this.container.nativeElement.offsetParent.parentElement.scrollTop = end;
+                clearInterval(interval);
+            }
+
+        }, 10);
+
+    }
+
+    public img(filename: string) : string {
+        return 'assets/img/' + filename;
+    }
 
     private detectBrowsers(): void {
         console.log(navigator.userAgent);
